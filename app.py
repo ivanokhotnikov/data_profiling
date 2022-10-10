4bcb5f18798e662850e204afd9e0bfc2c353c56d
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2022-10-10 14:08:43 +0100

    Reformatted with yapf, forecast data swapped with processed data

--- app.py
+++ app.py
@@ -15,22 +15,29 @@ ENGINEERED_FEATURES = [
 PRESSURE_TEMPERATURE = ['PT4', 'HSU IN', 'TT2', 'HSU OUT']
 VIBRATIONS = ['Vibration 1', ' Vibration 2']
 FORECAST_FEATURES = ENGINEERED_FEATURES + PRESSURE_TEMPERATURE + VIBRATIONS
+FORECAST_FEATURES = [f.strip().replace(' ', '_') for f in FORECAST_FEATURES]
 
-st.set_page_config(page_title='Test data profile', page_icon=os.path.join(os.path.dirname(__file__), 'images','fav.png'),layout='wide')
+st.set_page_config(
+    page_title='Test data profile',
+    page_icon=os.path.join(os.path.dirname(__file__), 'images', 'fav.png'),
+    layout='wide',
+)
+
 
 def main():
     st.title('Test data profile')
     is_minimal = st.checkbox('Minimal', False)
     storage_client = client.Client()
     bucket = storage_client.get_bucket('test_rig_data')
-    blob = bucket.get_blob('processed/forecast_data.csv')
+    blob = bucket.get_blob('processed/processed_data.csv')
     data_bytes = blob.download_as_bytes()
     df = pd.read_csv(io.BytesIO(data_bytes))
-    forecast_df = df[FORECAST_FEATURES].apply(
-                pd.to_numeric, errors='coerce', downcast='float')
+    forecast_df = df[FORECAST_FEATURES].apply(pd.to_numeric,
+                                              errors='coerce',
+                                              downcast='float')
     profile = ProfileReport(forecast_df, minimal=is_minimal, dark_mode=True)
     st_profile_report(profile)
-    
+
 
 if __name__ == "__main__":
     main()
