185cfb35f0bfb6b4943313e31d4ab7ad37a23fbc
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2022-10-25 10:11:13 +0100

    favicon location, typo corrected

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ FORECAST_FEATURES = [f.strip().replace(' ', '_') for f in FORECAST_FEATURES]
 
 st.set_page_config(
     page_title='Test data profile',
-    page_icon='https://github.com/ivanokhotnikov/test_rig/blob/master/images/fav.png?raw=True',
+    page_icon='https://github.com/ivanokhotnikov/test_rig_serving/blob/master/images/fav.png?raw=True',
     layout='centered',
 )
 
@@ -28,7 +28,7 @@ def main():
     is_minimal = st.checkbox('Minimal', False)
     storage_client = client.Client()
     bucket = storage_client.get_bucket('test_rig_processed_data')
-    blob = bucket.get_blob('/processed_data.csv')
+    blob = bucket.get_blob('processed_data.csv')
     data_bytes = blob.download_as_bytes()
     df = pd.read_csv(io.BytesIO(data_bytes))
     forecast_df = df[FORECAST_FEATURES].apply(pd.to_numeric,
